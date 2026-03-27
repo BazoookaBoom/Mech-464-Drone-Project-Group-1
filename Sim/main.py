f4a3bb8dc10c61283cf086d2d22819a94350cb4b
dylanRN <157074068+dylanRN@users.noreply.github.com>
2026-03-26 20:18:19 -0700

    Added cascaded PID. Still needs tuning and review angle conventions

--- Sim/main.py
+++ Sim/main.py
@@ -6,7 +6,13 @@ import time
 # Load scene
 model = mujoco.MjModel.from_xml_path('scene.xml')
 data = mujoco.MjData(model)
-
+# Initialize PID controller state
+x_errPrev = 0.0
+y_errPrev = 0.0
+z_errPrev = 0.0
+roll_errPrev = 0.0
+pitch_errPrev = 0.0
+dt = 0.0
 # 0.5 m cube centred at origin, bottom face at Z=0.5, top at Z=1.0
 WAYPOINTS = np.array([
     [-0.25, -0.25, 0.5],
@@ -73,21 +79,65 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
         step_start = time.time()
 
         # Advance waypoint when close enough
-        dist_to_wp = np.linalg.norm(data.qpos[:3] - target_pos)
-        if dist_to_wp < WAYPOINT_THRESHOLD:
-            wp_idx += 1
-            target_pos = WAYPOINTS[wp_idx % len(WAYPOINTS)].copy()
+        # dist_to_wp = np.linalg.norm(data.qpos[:3] - target_pos)
+        # if dist_to_wp < WAYPOINT_THRESHOLD:
+        #     # wp_idx += 1
+        #     target_pos = WAYPOINTS[wp_idx % len(WAYPOINTS)].copy()
+
+        # Set desired position
+        x_des = 0.0
+        y_des = 0.0
+        z_des = 0.5
 
         # PID controller
-        z_err = target_pos[2] - data.qpos[2]
+        # Calculate time step
+        dt = model.opt.timestep
+
+        # Set thrust to control altitude (Z)
+        z_err = z_des - data.qpos[2]
         z_vel = data.qvel[2]
         thrust = 0.26487 + (z_err * 3.0) - (z_vel * 1.5)
         data.ctrl[0] = np.clip(thrust, 0, 0.35)
 
-        x_err = target_pos[0] - data.qpos[0]
-        y_err = target_pos[1] - data.qpos[1]
-        data.ctrl[1] = (y_err * 0.05) - (data.qvel[1] * 0.02)   # x_moment (Roll)
-        data.ctrl[2] = -(x_err * 0.05) + (data.qvel[0] * 0.02)  # y_moment (Pitch)
+        # PID loop for desired axis angles (roll and pitch)
+        Kp_acc, Ki_acc, Kd_acc = 3.0, 0.0, 1.5
+        x_err = x_des - data.qpos[0]
+        y_err = y_des - data.qpos[1]
+        x_errI = x_errPrev + x_err*dt
+        y_errI = y_errPrev + y_err*dt
+        x_errI = np.clip(x_errI, -0.5, 0.5)
+        y_errI = np.clip(y_errI, -0.5, 0.5)
+        x_errPrev = x_err
+        y_errPrev = y_err
+        ax_des = (x_err * Kp_acc) + (x_errI * Ki_acc) - (data.qvel[0] * Kd_acc)
+        ay_des = (y_err * Kp_acc) + (y_errI * Ki_acc) - (data.qvel[1] * Kd_acc)
+
+        #Set desired roll and pitch based on desired accelerations
+        roll_des = 0.0 # np.clip(ay_des / 9.81, -0.5, 0.5)  # Roll controls lateral (Y) acceleration
+        pitch_des = 0.0 # np.clip(-ax_des / 9.81, -0.5, 0.5) # Pitch controls longitudinal (X) acceleration
+
+
+        # Calculate drone current roll and pitch from orientation quaternion
+        q = data.qpos[3:7]  # (w, x, y, z)
+        roll = np.arcsin(2*(q[0]*q[2] - q[3]*q[1]))  
+        pitch = - np.arctan2(2*(q[0]*q[2] - q[1]*q[3]), 1 - 2*(q[2]**2 + q[3]**2))
+
+        roll_err = roll_des - roll
+        pitch_err = pitch_des - pitch
+        roll_errI = roll_errPrev + roll_err*dt
+        pitch_errI = pitch_errPrev + pitch_err*dt
+        roll_D = data.qvel[3]  # roll rate
+        pitch_D = data.qvel[4]  # pitch rate
+        roll_errPrev = roll_err
+        pitch_errPrev = pitch_err
+        roll_errI = np.clip(roll_errI, -0.5, 0.5)
+        pitch_errI = np.clip(pitch_errI, -0.5, 0.5)
+
+        # Implement control through roll and pitch
+        disturbance = 0.5 if 5.0< data.time < 5.1 else 0.0
+        Kp, Ki, Kd = 15.0, 0.00, 0.0
+        data.ctrl[1] =  disturbance - np.clip(pitch_err * Kp + pitch_errI * Ki - pitch_D * Kd, -0.5, 0.5)  # x_moment (Pitch)
+        data.ctrl[2] = -np.clip(roll_err * Kp + roll_errI * Ki - roll_D * Kd, -0.5, 0.5) # y_moment (Roll)
         data.ctrl[3] = 0
 
         # Read sensors
@@ -107,5 +157,7 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
         viewer.sync()
 
         elapsed = time.time() - step_start
+
+
         if elapsed < model.opt.timestep:
             time.sleep(model.opt.timestep - elapsed)
